27e90a71f5d66d0029f5d9f4f2dd23bf1bb1e840
HigorMonteiro <higor.tecinfor@gmail.com>
2018-09-21 14:08:29 -0300

    fix name database

--- crawlers/crawlers/settings.py
+++ crawlers/crawlers/settings.py
@@ -89,4 +89,4 @@ ITEM_PIPELINES = {
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 MONGO_URI = 'localhost'
-MONGO_DATABASE = 'test_batabase'
+MONGO_DATABASE = 'database'
